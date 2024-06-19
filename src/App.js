de1dbec0c0c763230463246664b566642f3ead70
Leah Sun <leahs4@uci.edu>
2024-06-19 19:13:50 -0400

    finished styling the buttons as well as the color scheme for my website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderButtons from './HeaderButtons'; // Import the Header component
 
-function App() {
+function App(){
   return (
     <div className="App">
+      <HeaderButtons /> {/* Include the Header component here */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +23,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
