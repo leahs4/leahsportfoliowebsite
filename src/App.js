aa449545ec141e6a85250ada35f1e6886993a09d
Leah Sun <leahs4@uci.edu>
2024-06-19 22:11:22 -0400

    finished designing the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import logo from './logo.png'
 import './App.css';
+
 import HeaderButtons from './HeaderButtons'; // Import the Header component
 
 function App(){
@@ -7,18 +8,12 @@ function App(){
     <div className="App">
       <HeaderButtons /> {/* Include the Header component here */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={{ width: '50%', height: '50%' }} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p>
+            Welcome to Leah Sun's Portfolio Website (〜^∇^)〜
+          </p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
