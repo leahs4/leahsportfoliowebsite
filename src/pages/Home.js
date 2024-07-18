9f652b5f3bbb1bf5658516811970d9c70abd80b7
leahs4 <leahs4@uci.edu>
2024-07-18 16:50:35 -0700

    finished home animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.png';
 import "./Home.css"
+import 'animate.css'
 
 const Home=()=>{
   return(
     <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Leah Sun's Portfolio Website (〜^∇^)〜</p>
+        <img src={logo} className="App-logo animate__animated animate__bounceInDown" alt="logo" />
+        <p1 class="animate__animated animate__bounceInDown">Welcome to Leah Sun's Portfolio Website (〜^∇^)〜</p1>
       </header>
     </div>
   );
