d4f10abd6c134e5c545573705cfbd85d56f698bd
leahs4 <leahs4@uci.edu>
2024-07-18 13:37:47 -0700

    finished the contact me page layout

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import "./Contact.css"
+import React, { useEffect } from 'react';
+import "./Contact.css";
 
 const Contact=()=>{
+  useEffect(()=>{
+    const link = document.createElement("link");
+    link.href = "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+
+    return()=>{
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return(
     <div>
-      <header className="App-experience">
-        <h1>Contact</h1>
-        <p>This is my contact info</p>
+      <header className="App-contact">
+        <h1>Contact Me</h1>
+        <p>Please feel free to connect with me on LinkedIn!</p>
+        <i className='bx bxl-linkedin linkedin-icon'></i>
+        <p>For any inquiries please email me:</p>
+        <i className='bx bxl-gmail gmail-icon'></i>
       </header>
     </div>
   );
 }
+
 
 export default Contact;
