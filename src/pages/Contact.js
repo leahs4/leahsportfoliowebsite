a25f483a3a39e5f2e0b4aa7f1e42635c838ba3e8
leahs4 <leahs4@uci.edu>
2024-07-18 17:58:19 -0700

    finished animations for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import "./Contact.css";
+import 'animate.css'
 
 const Contact=()=>{
   useEffect(()=>{
@@ -15,14 +16,14 @@ const Contact=()=>{
   return(
     <div>
       <header className="App-contact">
-        <h1>Contact Me</h1>
-        <p>Please feel free to connect with me on LinkedIn!</p>
+        <h1 class="animate__animated animate__zoomIn">Contact Me</h1>
+        <p class="animate__animated animate__zoomIn">Please feel free to connect with me on LinkedIn!</p>
         <a href="https://www.linkedin.com/in/leahsun08/" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
-          <i className='bx bxl-linkedin-square'></i>
+          <i className='bx bxl-linkedin-square animate__animated animate__zoomIn'></i>
         </a>
-        <p>For any inquiries please email me:</p>
+        <p class="animate__animated animate__zoomIn">For any inquiries please email me:</p>
         <a href="mailto:leahs4@uci.edu" className="gmail-icon">
-          <i className='bx bxl-gmail'></i>
+          <i className='bx bxl-gmail animate__animated animate__zoomIn'></i>
         </a>
       </header>
     </div>
