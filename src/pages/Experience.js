f78c1b36ead9aa060edacac541fc55abd53b5c62
leahs4 <leahs4@uci.edu>
2024-07-18 20:00:32 -0700

    finished headers for resume, and project sections in the experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,11 +2,20 @@ import React from 'react';
 import "./Experience.css"
 const Experience=()=>{
   return(
-    <div>
+    <div class="experience-page">
       <header className="App-experience">
-        <h1>Experience</h1>
-        <p>This is my experience</p>
+        <h1 class="animate__animated animate__zoomIn">Experience</h1>
       </header>
+
+      <header className="projects">
+        <h1 class="animate__animated animate__zoomIn">Projects</h1>
+        <h1 class="animate__animated animate__zoomIn">Resume</h1>
+      </header>
+      
+
+      <div class="experience-container">  
+      </div>
+        
     </div>
   );
 }
