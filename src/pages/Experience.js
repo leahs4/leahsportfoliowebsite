4439d40a495cff853d07dc281ca16f285cefbf3c
leahs4 <leahs4@uci.edu>
2024-07-20 01:27:48 -0700

    made two project buttons

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Experience.css"
+
 const Experience=()=>{
   return(
     <div class="experience-page">
@@ -11,9 +12,21 @@ const Experience=()=>{
         <h1 class="animate__animated animate__zoomIn">Projects</h1>
         <h1 class="animate__animated animate__zoomIn">Resume</h1>
       </header>
-      
+
 
       <div class="experience-container">  
+        
+        <li>
+          <a href="https://devpost.com/software/munch-rh24a6" className="button-01" target="_blank" rel="noopener noreferrer"> 
+            Hack@UCI 2023 Winner: Munch
+          </a>
+        </li>
+        <li>
+          <a href="https://devpost.com/software/property-guessr" className="button-02" target="_blank" rel="noopener noreferrer"> 
+            Hack@UCI 2024: Property Guessr
+          </a>
+        </li>
+        
       </div>
         
     </div>
@@ -21,3 +34,4 @@ const Experience=()=>{
 }
 
 export default Experience;
+
