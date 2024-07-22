afbf18f4204bd8b2cf059121dfed46f611d12dbe
leahs4 <leahs4@uci.edu>
2024-07-22 01:13:53 -0700

    finished pdf to resume, but not functionality

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {useEffect} from 'react';
 import "./Experience.css"
 
 const Experience=()=>{
+  useEffect(()=>{
+    const link = document.createElement("link");
+    link.href = "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+    return()=>{
+      document.head.removeChild(link);
+    };
+  }, []);
   return(
     <div className="experience-page">
       <header className="App-experience">
@@ -12,19 +22,23 @@ const Experience=()=>{
         <div className="projects">
           <h1 className="animate__animated animate__zoomIn">Projects</h1>
           <div className="buttons-container">
-            <a href="https://devpost.com/software/munch-rh24a6" className="button" target="_blank" rel="noopener noreferrer"> 
+            <a href="https://leahhsun.notion.site/Discord-Bot-fa591e2074124b0dbf61707f2461726e" className="button animate__animated animate__zoomIn" target="_blank" rel="noopener norefferer">
+              ExcuserBot Discord Bot 2023
+            </a>
+            <a href="https://devpost.com/software/munch-rh24a6" className="button animate__animated animate__zoomIn" target="_blank" rel="noopener noreferrer"> 
               Hack@UCI 2023 Winner: Munch
             </a>
-            <a href="https://devpost.com/software/property-guessr" className="button" target="_blank" rel="noopener noreferrer"> 
+            <a href="https://devpost.com/software/property-guessr" className="button animate__animated animate__zoomIn" target="_blank" rel="noopener noreferrer"> 
               Hack@UCI 2024: Property Guessr
             </a>
-            <a href="https://devpost.com/software/tubetastic" className="button" target="_blank" rel="noopener norefferer">
+            <a href="https://devpost.com/software/tubetastic" className="button animate__animated animate__zoomIn" target="_blank" rel="noopener norefferer">
               LA Hacks 2024: TubeTasTic
             </a>
           </div>
         </div>
         <div className="resume">
           <h1 className="animate__animated animate__zoomIn">Resume</h1>
+          <i className='bx bxs-file-pdf animate__animated animate__zoomIn'></i>
         </div>
       </div>
     </div>
