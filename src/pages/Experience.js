9cd0449cfd8ca1d7493bcd74591750a706dc80c5
leahs4 <leahs4@uci.edu>
2024-07-21 21:03:21 -0700

    fixed buttons to be more aligned

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,35 +3,32 @@ import "./Experience.css"
 
 const Experience=()=>{
   return(
-    <div class="experience-page">
+    <div className="experience-page">
       <header className="App-experience">
-        <h1 class="animate__animated animate__zoomIn">Experience</h1>
+        <h1 className="animate__animated animate__zoomIn">Experience</h1>
       </header>
 
-      <header className="projects">
-        <h1 class="animate__animated animate__zoomIn">Projects</h1>
-        <h1 class="animate__animated animate__zoomIn">Resume</h1>
-      </header>
-
-
-      <div class="experience-container">  
-        
-        <li>
-          <a href="https://devpost.com/software/munch-rh24a6" className="button-01" target="_blank" rel="noopener noreferrer"> 
-            Hack@UCI 2023 Winner: Munch
-          </a>
-        </li>
-        <li>
-          <a href="https://devpost.com/software/property-guessr" className="button-02" target="_blank" rel="noopener noreferrer"> 
-            Hack@UCI 2024: Property Guessr
-          </a>
-        </li>
-        
+      <div className="projects-section">
+        <div className="projects">
+          <h1 className="animate__animated animate__zoomIn">Projects</h1>
+          <div className="buttons-container">
+            <a href="https://devpost.com/software/munch-rh24a6" className="button" target="_blank" rel="noopener noreferrer"> 
+              Hack@UCI 2023 Winner: Munch
+            </a>
+            <a href="https://devpost.com/software/property-guessr" className="button" target="_blank" rel="noopener noreferrer"> 
+              Hack@UCI 2024: Property Guessr
+            </a>
+            <a href="https://devpost.com/software/tubetastic" className="button" target="_blank" rel="noopener norefferer">
+              LA Hacks 2024: TubeTasTic
+            </a>
+          </div>
+        </div>
+        <div className="resume">
+          <h1 className="animate__animated animate__zoomIn">Resume</h1>
+        </div>
       </div>
-        
     </div>
   );
 }
 
 export default Experience;
-
