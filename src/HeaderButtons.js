b72fb7ac9d3dc13707dc33aba0dde92ff389fa16
leahs4 <leahs4@uci.edu>
2024-08-02 16:45:58 -0700

    finished creating hamburger button functionality

--- src/HeaderButtons.js
+++ src/HeaderButtons.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import './HeaderButtons.css';
+import 'animate.css';
 
 const HeaderButtons=()=>{
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav=()=>{
+    setIsNavOpen(!isNavOpen);
+  }
+
+  const closeNav=()=>{
+    setIsNavOpen(false);
+  }
+  
   return(
     <header className="header">
       <nav>
-        <ul className="nav-list">
-          <li><Link to="/home" className="nav-button">Home</Link></li>
-          <li><Link to="/about" className="nav-button">About Me</Link></li>
-          <li><Link to="/experience" className="nav-button">Experience</Link></li>
-          <li><Link to="/contact" className="nav-button">Contact</Link></li>
+        <div className="hamburger" onClick={toggleNav}>
+          &#9776;
+        </div>
+        <ul className={`nav-list ${isNavOpen ? 'open animate__animated animate__fadeIn' : ''}`}>
+          <li><Link to="/home" className="nav-button" onClick={closeNav}>Home</Link></li>
+          <li><Link to="/about" className="nav-button" onClick={closeNav}>About Me</Link></li>
+          <li><Link to="/experience" className="nav-button" onClick={closeNav}>Experience</Link></li>
+          <li><Link to="/contact" className="nav-button" onClick={closeNav}>Contact</Link></li>
         </ul>
       </nav>
     </header>
