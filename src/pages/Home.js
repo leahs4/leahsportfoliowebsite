bc32ae96606c2b96432b8280ed86f3caa6f3a6c1
leahs4 <leahs4@uci.edu>
2024-08-02 17:28:17 -0700

    fixed phone scaling for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,8 @@ const Home=()=>{
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo animate__animated animate__bounceInDown" alt="logo" />
-        <p1 class="animate__animated animate__bounceInDown">Welcome to Leah Sun's Portfolio Website (〜^∇^)〜</p1>
+        <p1 class= "animate__animated animate__bounceInDown main-text">Welcome to Leah Sun's Portfolio Website</p1>
+        <p1 class= "animate__animated animate__bounceInDown main-text">(〜^∇^)〜</p1>
       </header>
     </div>
   );
