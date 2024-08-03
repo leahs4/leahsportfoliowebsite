706b6ebe68568e360d9071395e3beb53a8d99acd
leahs4 <leahs4@uci.edu>
2024-08-02 17:59:30 -0700

    added mobile scalability to contact me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,11 +17,11 @@ const Contact=()=>{
     <div>
       <header className="App-contact">
         <h1 class="animate__animated animate__zoomIn">Contact Me</h1>
-        <p class="animate__animated animate__zoomIn">Please feel free to connect with me on LinkedIn!</p>
+        <p class="pls animate__animated animate__zoomIn">Please feel free to connect with me on LinkedIn!</p>
         <a href="https://www.linkedin.com/in/leahsun08/" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
           <i className='bx bxl-linkedin-square animate__animated animate__zoomIn'></i>
         </a>
-        <p class="animate__animated animate__zoomIn">For any inquiries please email me:</p>
+        <p class="inq animate__animated animate__zoomIn">For any inquiries please email me:</p>
         <a href="mailto:leahs4@uci.edu" className="gmail-icon">
           <i className='bx bxl-gmail animate__animated animate__zoomIn'></i>
         </a>
